70fd56f0dd9016f32f3aa04c93b9543a239d855e
pamella-silva <90103227+pampamella@users.noreply.github.com>
2022-05-28 15:47:24 -0300

    remoção de outras linguagens

--- IDL/timeSeriesRun.pro
+++
@@ -1,5 +0,0 @@
-PRINT, 'Job started'
-
-test_time_series_aoa
-
-PRINT, 'Job complete'
--- Matlab/test_fibonacci_recur.m
+++
@@ -1,9 +0,0 @@
-
-% Matlab: Recursive fibonacci
-function f=test_fibonacci_recur(n)
-    if n <= 2
-        f = 1.0;
-    else
-        f = test_fibonacci_recur(n-1) + test_fibonacci_recur(n-2);
-    end
-end
